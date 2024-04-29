41b58aba3cf9105f83f39352b3b8f48201240f69
SantiagoAO <164049042+SantiagoAO@users.noreply.github.com>
2024-04-28 19:52:41 -0600

    Update form.js

--- form.js
+++ form.js
@@ -1,51 +1,99 @@
-
 function getMakeID() {
     let make_value = document.options_motorleads.makes.value;
-    document.options_motorleads.model.disabled = false;
-    
-    console.log(make_value);
-    let url = "http://localhost/RETO/form.php?make_id="+make_value;
-
-    window.history.pushState({}, '', url);
-    location.reload();
-
+    if (make_value !== "null") {
+        document.options_motorleads.model.disabled = false;
+        updateURL();
+    }
+    else {
+        document.options_motorleads.model.disabled = true;
+        document.options_motorleads.year.disabled = true;
+        document.options_motorleads.version.disabled = true;
+    }
 }
 
 function getModelID() {
-    let make_value = document.options_motorleads.makes.value;
     let model_value = document.options_motorleads.model.value;
-    document.options_motorleads.year.disabled = false;
-    
-    console.log(model_value);
-    let url = "http://localhost/RETO/form.php?make_id="+make_value+"&model_id="+model_value;
-
-    window.history.pushState({}, '', url);
-    location.reload();
+    if (model_value !== "null") {
+        document.options_motorleads.year.disabled = false;
+        updateURL();
+    }
+    else {
+        document.options_motorleads.year.disabled = true;
+        document.options_motorleads.version.disabled = true;
+    }
 }
 
 function getYearID() {
-    let make_value = document.options_motorleads.makes.value;
-    let model_value = document.options_motorleads.model.value;
     let year_value = document.options_motorleads.year.value;
-    document.options_motorleads.version.disabled = false;
-    
-    console.log(model_value);
-    let url = "http://localhost/RETO/form.php?make_id="+make_value+"&model_id="+model_value+"&year_id="+year_value;
-
-    window.history.pushState({}, '', url);
-    location.reload();
+    if (year_value !== "null") {
+        document.options_motorleads.version.disabled = false;
+        updateURL();
+    } else {
+        document.options_motorleads.version.disabled = true;
+    }
 }
 
 function getVersionID() {
+    let version_value = document.options_motorleads.version.value;
+    if (version_value !== "null") {
+        document.options_motorleads.color.disabled = false;
+        updateURL();
+    }
+    else {
+        document.options_motorleads.color.disabled = true;
+    }
+}
+
+function updateURL() {
     let make_value = document.options_motorleads.makes.value;
     let model_value = document.options_motorleads.model.value;
     let year_value = document.options_motorleads.year.value;
     let version_value = document.options_motorleads.version.value;
-    document.options_motorleads.color.disabled = false;
-    
-    console.log(model_value);
-    let url = "http://localhost/RETO/form.php?make_id="+make_value+"&model_id="+model_value+"&year_id="+year_value+"&version_id="+version_value;
+
+    let url = "http://localhost/RETO/form.php?make_id=" + make_value;
+
+    if (model_value !== "null") {
+        url += "&model_id=" + model_value;
+    }
+    if (year_value !== "null") {
+        url += "&year_id=" + year_value;
+    }
+    if (version_value !== "null") {
+        url += "&version_id=" + version_value;
+    }
 
     window.history.pushState({}, '', url);
-    location.reload();
+    window.location.reload();
 }
+
+function saveSelectedValues() {
+    localStorage.setItem('make_value', document.options_motorleads.makes.value);
+    localStorage.setItem('model_value', document.options_motorleads.model.value);
+    localStorage.setItem('year_value', document.options_motorleads.year.value);
+    localStorage.setItem('version_value', document.options_motorleads.version.value);
+}
+
+function restoreSelectedValues() {
+    document.options_motorleads.makes.value = localStorage.getItem('make_value') || "null";
+    document.options_motorleads.model.value = localStorage.getItem('model_value') || "null";
+    document.options_motorleads.year.value = localStorage.getItem('year_value') || "null";
+    document.options_motorleads.version.value = localStorage.getItem('version_value') || "null";
+    
+    if (document.options_motorleads.makes.value !== "null") {
+        document.options_motorleads.model.disabled = false;
+    }
+    if (document.options_motorleads.model.value !== "null") {
+        document.options_motorleads.year.disabled = false;
+    }
+    if (document.options_motorleads.year.value !== "null") {
+        document.options_motorleads.version.disabled = false;
+    }
+}
+
+window.onload = function() {
+    restoreSelectedValues();
+};
+
+window.onbeforeunload = function() {
+    saveSelectedValues();
+};
